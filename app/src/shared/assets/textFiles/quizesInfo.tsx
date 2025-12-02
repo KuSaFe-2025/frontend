9641c0f2c56edc53edb4d56d672a44d1767be0c4
Самохвалов Андрей Ремович <a.samohvalov@aitiguru.com>
2025-12-02 09:22:08 +0300

    chore: update styles

--- app/src/shared/assets/textFiles/quizesInfo.tsx
+++ app/src/shared/assets/textFiles/quizesInfo.tsx
@@ -54,4 +54,50 @@ export const quizInfoArray: ProjectInfo[] = [
     ),
     showButton: false,
   },
+  {
+    sysname: 'lorem',
+    title: 'Lorem Project',
+    description: (
+      <>
+        <strong>Lorem ipsum dolor sit amet</strong>, consectetur adipiscing elit.
+        <br />
+        <br />
+        <strong>Aliquam</strong> dignissim orci sed <strong>massa</strong> ultrices, a placerat{' '}
+        <strong>nibh</strong> tristique.
+        <br />
+        <br />
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        <strong> Integer</strong> vulputate, velit vel cursus varius, nisl urna tincidunt nunc, at
+        tristique <strong>odio</strong> sapien vel lorem.
+        <br />
+        <br />
+        <strong>Sed euismod</strong> magna et dolor imperdiet, vitae gravida ligula dictum. Donec
+        dictum risus vitae ex <strong>malesuada</strong> porttitor.
+      </>
+    ),
+    showButton: true,
+  },
+  {
+    sysname: 'lorem',
+    title: 'Lorem Project',
+    description: (
+      <>
+        <strong>Lorem ipsum dolor sit amet</strong>, consectetur adipiscing elit.
+        <br />
+        <br />
+        <strong>Aliquam</strong> dignissim orci sed <strong>massa</strong> ultrices, a placerat{' '}
+        <strong>nibh</strong> tristique.
+        <br />
+        <br />
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        <strong> Integer</strong> vulputate, velit vel cursus varius, nisl urna tincidunt nunc, at
+        tristique <strong>odio</strong> sapien vel lorem.
+        <br />
+        <br />
+        <strong>Sed euismod</strong> magna et dolor imperdiet, vitae gravida ligula dictum. Donec
+        dictum risus vitae ex <strong>malesuada</strong> porttitor.
+      </>
+    ),
+    showButton: false,
+  },
 ];
